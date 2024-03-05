add07632d05fcec88bb29f21abd4b867d613c823
botLx <alex.schnabl02@gmail.com>
2024-03-05 18:06:36 +0100

    Added Ui

--- app/src/main/java/com/example/se2_ab0/MainActivity.kt
+++ app/src/main/java/com/example/se2_ab0/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.se2_ab0
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.se2_ab0.ui.theme.SE2_AB0Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +30,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    var text = "";
+
+
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Geben Sie Ihre Matrikelnummer ein")
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Matrikelnummer") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+          /* TODO: Add TCP Call */
+        }) {
+            Text("Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
